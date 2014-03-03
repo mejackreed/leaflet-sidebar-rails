db320e3a108e2a08da6452ec8607ba6113408b16
Jack Reed <pjreed@stanford.edu>
2014-03-03 10:22:00 -0800

    initial commit

---
+++ lib/leaflet-sidebar-rails.rb
@@ -0,0 +1,10 @@
+require "leaflet-sidebar-rails/version"
+
+module Leaflet
+  module Sidebar
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/leaflet-sidebar-rails/version.rb
@@ -0,0 +1,7 @@
+module Leaflet
+  module Sidebar
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
